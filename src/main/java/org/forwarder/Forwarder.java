9acea128079ec65a53a4f8c3f115b6921ad94327
formaten <formaten@users.noreply.github.com>
2019-12-17 17:20:45 +0800

    Added license declaration and changed Executor usage in Config
    1. Added license declaration;
    2. Changed Executor usage in Config;

--- src/main/java/org/forwarder/Forwarder.java
+++ src/main/java/org/forwarder/Forwarder.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.forwarder;
 
 import java.io.FileNotFoundException;
@@ -6,8 +22,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import javax.naming.OperationNotSupportedException;
 
 import org.forwarder.backend.BackendFactory;
 import org.forwarder.backend.BackendLoader;
@@ -57,8 +71,10 @@ public class Forwarder implements AutoCloseable {
 		}
 	}
 
-	public Forwarder executor(String name) throws OperationNotSupportedException {
-		this.executor = ExecutorFactory.createInstance(model, name);
+	public Forwarder executor(Class<? extends Executor> classOfExecutor)
+			throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+		this.executor = ExecutorFactory.createInstance(model, classOfExecutor);
 		return this;
 	}
 
--- src/main/java/org/forwarder/executor/ExecutorFactory.java
+++ src/main/java/org/forwarder/executor/ExecutorFactory.java
@@ -1,23 +1,33 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.forwarder.executor;
 
-import javax.naming.OperationNotSupportedException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
-import org.forwarder.executor.impls.RayExecutor;
-import org.forwarder.executor.impls.RecursionExecutor;
 import org.onnx4j.Model;
 
 public class ExecutorFactory {
 
-	public static Executor<?> createInstance(Model model, String executorName)
-			throws OperationNotSupportedException {
-		if ("ray".equalsIgnoreCase(executorName)) {
-			return new RayExecutor<Object>(model);
-		} else if ("recursion".equalsIgnoreCase(executorName)) {
-			return new RecursionExecutor<Object>(model);
-		} else {
-			throw new OperationNotSupportedException(
-					String.format("Executor named \"%s\" not supported", executorName));
-		}
+	public static Executor<?> createInstance(Model model, Class<? extends Executor> classOfExecutor)
+			throws NoSuchMethodException, SecurityException, InstantiationException,
+			IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Constructor<? extends Executor> c = classOfExecutor.getConstructor(Model.class);
+		return c.newInstance(model);
 	}
 
 }
